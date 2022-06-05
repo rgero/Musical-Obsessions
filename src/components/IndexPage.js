c0a859d64c79ce3701e1a2d7325e02678663aa6a
Roy Gero <roy.gero.ii@gmail.com>
2022-06-04 22:25:40 -0400

    Changing the Index Page to a class so I can use component did mount to set the state. Now I just need to redirect the user to the playlist options when they are logged in.

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,53 +1,72 @@
 import React from 'react';
-import {useEffect, useState} from 'react';
 import Header from './Header';
 
-import Credientials from "../Credentials";
+import Credentials from '../Credentials';
 
-var AUTH_ENDPOINT = Credientials["AUTH_ENDPOINT"];
-var CLIENT_ID = Credientials["API_KEY"]
-var REDIRECT_URI = Credientials["REDIRECT_URL"]
-var RESPONSE_TYPE = "token";
-var authURL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
-
-function IndexPage() 
+export class IndexPage extends React.Component
 {
-  const [token, setToken] = useState("")
+    constructor(props)
+    {
+        super(props)
 
-  useEffect(() => {
-      const hash = window.location.hash
-      let token = window.localStorage.getItem("token")
+        // Class Variables
+        var AUTH_ENDPOINT = Credentials["AUTH_ENDPOINT"];
+        var CLIENT_ID = Credentials["API_KEY"]
+        var REDIRECT_URI = Credentials["REDIRECT_URL"]
+        var RESPONSE_TYPE = "token";
+        this.authURL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
 
-      if (!token && hash) {
-          token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+        this.parseToken = this.parseToken.bind(this);
 
-          window.location.hash = ""
-          window.localStorage.setItem("token", token)
-      }
+        this.state = {
+            token : this.props.token ? this.props.token : ""
+        }
+    }
 
-      setToken(token)
+    componentDidMount()
+    {
+        this.parseToken()
+    }
 
-  }, [])
+    parseToken()
+    {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+  
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+  
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+  
+        this.setState(()=>({
+            token : token
+        }))
+    }
 
-  return(
-    <div>
-      <Header/> 
-      <div className='content-container'>
-        <div className='content-container__title'>
-          Welcome to Musical Obsessions
-        </div>
-        <div>
-            This purpose of this website is to create a Spotify Playlist of your most listened songs over the a set period of time.
-        </div>
-        <div>
-            <a className='button--spotify' href={authURL}>Login to Spotify!</a>
-        </div>
-        <div>
-        </div>
-      </div>
-    </div>
-  )
-
+    render()
+    {
+        return(
+            <div>
+              <Header/> 
+              <div className='content-container'>
+                <div className='content-container__title'>
+                  Welcome to Musical Obsessions
+                </div>
+                <div>
+                    This purpose of this website is to create a Spotify Playlist of your most listened songs over the a set period of time.
+                </div>
+                <div>
+                    <a className='button--spotify' href={this.authURL}>Login to Spotify!</a>
+                </div>
+                <div>
+                    The Token is here: {this.state.token};
+                </div>
+              </div>
+            </div>
+          )
+    }
 }
 
 export default IndexPage;
