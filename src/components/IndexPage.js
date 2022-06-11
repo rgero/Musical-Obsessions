81f9f59b6b3572b42f7d88fd68910e45df46aa95
Roy Gero <roy.gero.ii@gmail.com>
2022-06-11 10:06:25 -0400

    Changing from the credentials file to the actual way I should be storing this stuff.

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Credentials from '../Credentials';
 
 import PublicPage from './PublicPage';
 import OptionsPage from './OptionsPage';
@@ -11,9 +10,9 @@ export class IndexPage extends React.Component
         super(props)
 
         // Class Variables
-        var AUTH_ENDPOINT = Credentials["AUTH_ENDPOINT"];
-        var CLIENT_ID = Credentials["API_KEY"]
-        var REDIRECT_URI = Credentials["REDIRECT_URL"]
+        var AUTH_ENDPOINT = process.env.REACT_APP_AUTH_ENDPOINT;
+        var CLIENT_ID = process.env.REACT_APP_API_KEY;
+        var REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
         var RESPONSE_TYPE = "token";
         var scope = "playlist-modify-public playlist-modify-private"
         this.authURL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
